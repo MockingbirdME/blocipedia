b36a8de348bd51b796fcb44282b4c4413c6d151e
Devon Henegar <devon.henegar@gmail.com>
2016-06-30 11:23:23 -0400

    added user roles and Pundit for authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,26 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include Pundit
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   before_action :authenticate_user!
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
 
   protect_from_forgery with: :exception
 
+
+private
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
   end
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
 
+
   has_many :wikis
+
+  enum role: [:standard, :premium, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :standard
+  end
 
 
 
